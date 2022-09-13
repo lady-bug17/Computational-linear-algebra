8354bd7e417c8a9e6fb25dcdbce735d55d65088c
Вікторія Лоїк <57986339+lady-bug17@users.noreply.github.com>
2022-09-13 23:44:59 +0300

    Add files via upload

---
+++ Jacobi/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Task2
+{
+    public partial class Form1 : Form
+    {
+        private Matrix m = new Matrix("m.txt");
+        private Vector v = new Vector("v.txt");
+        private Vector approximationVector = new Vector("approx.txt");
+
+        public Form1()
+        {
+            InitializeComponent();
+            m.AddToDataGridView(dataGridView1);
+            v.AddToDataGridView(dataGridView2);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Jacobi.IsConvergent(m))
+            {
+                Matrix iterations = new Matrix();
+                Vector result = Jacobi.CalculateJacobi(m, v, approximationVector, iterations);
+                result.AddToDataGridView(dataGridView4);
+                iterations.AddToDataGridView(dataGridView3);
+            }
+            else
+            { 
+                MessageBox.Show("Matrix is not convergent");
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Jacobi/Jacobi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Task2
+{
+    class Jacobi
+    {
+        public static bool IsConvergent(Matrix m)
+        {
+            for (int i = 0; i < m.m; i++)
+            {
+                double sum = m.matrix[i].Sum(x => Math.Abs(x)) - Math.Abs(m.matrix[i][i]);
+                if (Math.Abs(m.matrix[i][i]) <= sum)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static Vector CalculateJacobi(Matrix a, Vector b, Vector x, Matrix iterations)
+        {
+            Vector result = new Vector(b.size);
+            double maxDifference = 0;
+            for (int i = 0; i < a.n; i++)
+            {
+                result.elements[i] = b.elements[i] / a.matrix[i][i];
+                for (int j = 0; j < a.m; j++)
+                {
+                    if (i != j)
+                    {
+                        result.elements[i] -= a.matrix[i][j] * x.elements[j] / a.matrix[i][i];
+                    }
+                }
+            }
+            for (int i = 0; i < b.size; i++)
+            {
+                maxDifference = Math.Max(maxDifference, Math.Abs(x.elements[i] - result.elements[i]));
+            }
+            if (maxDifference > 1e-3)
+            {
+                x = result;
+                iterations.matrix.Add(result.elements);
+                iterations.m = a.m;
+                iterations.n++;
+                result = CalculateJacobi(a, b, x, iterations);
+            }
+            return result;
+        }
+    }
+}
