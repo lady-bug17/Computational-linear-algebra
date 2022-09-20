23ad055e7cf86e6b3c24796143ead881db8cf319
Вікторія Лоїк <57986339+lady-bug17@users.noreply.github.com>
2022-09-21 01:09:01 +0300

    Add files via upload

---
+++ Kathil-Maki/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task2
+{
+    public partial class Form1 : Form
+    {
+        private Matrix m = new Matrix("m.txt");
+        private Vector v = new Vector("v.txt");
+        //Matrix m = new Matrix(5, 5);
+        //Vector v = new Vector(5);
+
+        public Form1()
+        {
+            //m.RandomizeMatrix();
+            //v.RandomizeVector();
+            InitializeComponent();
+            //m.AddToDataGridView(dataGridView1);
+            //v.AddToDataGridView(dataGridView2);
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<int> res = Сuthill_McKee.FindPeripheralVertex(m);
+                dataGridView1.RowCount = m.m;
+                for (int i = 0; i < res.Count; i++)
+                {
+                    dataGridView1.Rows[i].Cells[0].Value = res[i] + 1;
+                }
+                var res69 = Сuthill_McKee.CalculateСuthillMcKee(m, 4);
+                dataGridView2.RowCount = m.m;
+                for (int i = 0; i < res69.Count; i++)
+                {
+                    dataGridView2.Rows[i].Cells[0].Value = res69[i] + 1;
+                }
+                Matrix dublicate = new Matrix(m.n, m.m);
+                for (int i = 0; i < m.m; i++)
+                {
+                    for (int j = 0; j < m.m; j++)
+                    {
+                        dublicate.matrix[i][j] = m.matrix[res69[i]][res69[j]];
+                    }
+                }
+                dublicate.AddToDataGridView(dataGridView4);
+                m.AddToDataGridView(dataGridView3);
+                //using (StreamWriter outputFile = new StreamWriter("f.txt"))
+                //{
+                //    for (int i = 0; i < m.n; i++)
+                //    {
+                //        for (int j = 0; j < m.m; j++)
+                //        {
+                //            outputFile.Write(dublicate.matrix[i][j]);
+                //            outputFile.Write(" ");
+                //        }
+                //        outputFile.WriteLine();
+                //    }
+                //}
+                Vector data = new Vector("v.txt");
+                Matrix decomposed = Gauss.RowReduction(m, v);
+                Matrix inverse = Gauss.FindInverseMatrix(decomposed);
+                Vector result = inverse * v;
+                result.AddToDataGridView(dataGridView5);
+                Vector dub = new Vector(data.elements);
+                for (int i = 0; i < m.n; i++)
+                {
+                    dub.elements[i] = data.elements[res69[i]];
+                }
+                //using (StreamWriter outputFile = new StreamWriter("g.txt"))
+                //{
+                //    for (int i = 0; i < m.n; i++)
+                //    {
+                //        for (int j = 0; j < m.m; j++)
+                //        {
+                //            outputFile.Write(dublicate.matrix[i][j]);
+                //            outputFile.Write(" ");
+                //        }
+                //        outputFile.WriteLine();
+                //    }
+                //}
+                decomposed = Gauss.RowReduction(dublicate, dub);
+                result = Gauss.ReverseWay(dublicate, dub);
+                //using (StreamWriter outputFile = new StreamWriter("f.txt"))
+                //{
+                //    outputFile.WriteLine();
+                //    for (int i = 0; i < m.n; i++)
+                //    {
+                //        outputFile.Write(result.elements[i]);
+                //        outputFile.Write(" ");
+                //    }
+                //    outputFile.WriteLine();
+
+                //}
+
+                Vector ans = new Vector(result.elements);
+                for (int i = 0; i < m.n; i++)
+                {
+                    ans.elements[res69[i]] = result.elements[i];
+                }
+                ans.AddToDataGridView(dataGridView5);
+            }
+            catch (InvalidDataException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Kathil-Maki/Kathil-Maki.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Task2
+{
+    class Сuthill_McKee
+    {
+        public static List<int> FindPeripheralVertex(Matrix m)
+        {
+            int iter = 0;
+            Matrix copy = m;
+            Matrix prevCopy = copy;
+            for (int i = 0; i < m.n; i++)
+            {
+                for (int j = 0; j < m.n; j++)
+                {
+                    while (copy.matrix[i][j] == 0)
+                    {
+                        prevCopy = copy;
+                        copy = Matrix.MultiplyByOrRule(copy, m);
+                        iter++;
+                    }
+                }
+            }
+            List<int> result = new List<int>();
+            for (int i = 0; i < m.n; i++)
+            {
+                for (int j = 0; j < m.n; j++)
+                {
+                    if(prevCopy.matrix[i][j] == 0)
+                    {
+                        result.Add(i);
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static List<int> CalculateСuthillMcKee(Matrix m, int peripheralVertex)
+        {
+            List<bool> used = new List<bool>();
+            List<int> result = new List<int>();
+            List<int> vertecesDegree = new List<int>();
+            for (int i = 0; i < m.n; i++)
+            {
+                int degree = 0;
+                for (int j = 0; j < m.n; j++)
+                {
+                    if (m.matrix[i][j] == 1)
+                    {
+                        degree++;
+                    }
+                }
+                vertecesDegree.Add(degree - 1);
+            }
+            for (int i = 0; i < m.n; i++)
+            {
+                used.Add(false);
+            }
+            used[peripheralVertex] = true;
+            result.Add(peripheralVertex);
+            List<int> last = result;
+            Dictionary<int, int> l = new Dictionary<int, int>();
+            while(result.Count != m.n)
+            {
+                List<KeyValuePair<int, int>> need = new List<KeyValuePair<int, int>>();
+                foreach (var vertex in last)
+                {
+                    for (int i = 0; i < m.n; i++)
+                    {
+                        if (m.matrix[vertex][i] == 1 && !used[i])
+                        {
+                            need.Add(new KeyValuePair<int, int>(i, vertecesDegree[i]));
+                            used[i] = true;
+                        }
+                    }
+                }
+                need.Sort((x, y) => x.Value.CompareTo(y.Value));
+                foreach(var p in need)
+                {
+                    result.Add(p.Key);
+                }
+                last = need.Select(x => x.Key).ToList();
+                
+            }
+            return result;
+        }
+    }
+}
